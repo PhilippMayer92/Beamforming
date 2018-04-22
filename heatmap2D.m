58137f63fda7223e09c15ec8d8e38c164789b6ee
Philipp Mayer <mayerph@stud.sbg.ac.at>
2018-04-22 17:01:48 +0200

    some comments

--- heatmap2D.m
+++ heatmap2D.m
@@ -1,6 +1,10 @@
 % heatmap for 2D beamforming
+% basic assumptions:
+%   waves spread symmetric to its source
+%   there is no damping
 clear;
 
+% space and time parameters
 dx = 0.5;
 x_min = -20;
 x_max = 120;
@@ -16,11 +20,17 @@ t_max = 2*pi;
 [x, y] = meshgrid(x_min:dx:x_max,y_min:dy:y_max);
 lambda = 2*pi;
 
+% target coordinates
 x_target = 50;
 y_target = 50;
-x_sources = [0 25 39 64 78 99];
-y_sources = [45 12 78 64 91 8];
+% coordinates of the wave sources
+x_sources = [0 25 39 64 78 99 23 71 10 81];
+y_sources = [45 12 78 64 91 8 11 49 51 23];
 nr_sources = size(x_sources, 2);
+
+% compute distance between target and each source thereby the
+% necessary phaseshift for each source to get an interference maximum
+% at the target
 distance = sqrt((x_target-x_sources).^2+(y_target-y_sources).^2);
 phaseshift = distance-floor(distance/lambda)*lambda;
 
@@ -28,12 +38,15 @@ minimum = zeros(size(x, 1), size(y, 1));
 maximum = zeros(size(x, 1), size(y, 1));
 for t = t_min:dt:t_max+dt
     all = 0;
+    % compute each wave and overlay
     for i = 1:nr_sources
+        % compute positions relative to wave source
         positions = sqrt((x-x_sources(i)).^2+(y-y_sources(i)).^2);
         part = sin(positions-t-phaseshift(i));
         all = all+part;
     end
     
+    % check for each point, whether the overlay reaches a new min/max
     if t == 1
         minimum = all;
         maximum = all;
@@ -42,7 +55,7 @@ for t = t_min:dt:t_max+dt
         maximum = getMaxMatrix(maximum, all);
     end
 end
-
+% compute amplitude at each point
 amp = maximum-minimum;
 
 hold on
@@ -51,12 +64,14 @@ plot3([x_target x_target], [y_target y_target], [0 2*nr_sources+1], 'r');
 axis([x_min x_max y_min y_max 0 2*nr_sources+1]);
 hold off
 
+% find amplitude at target point
 k_x = (x_target-x_min)/dx+1;
 k_y = (y_target-y_min)/dy+1;
 amp_target = amp(k_x, k_y);
 sprintf('Amplitude %f at target point (%d, %d)', amp_target, x_target, y_target)
 
 
+% find minimum for each position
 function y = getMinMatrix(x1, x2)
     y = zeros(size(x1));
     
@@ -67,6 +82,7 @@ function y = getMinMatrix(x1, x2)
     end
 end
 
+% find maximum for each position
 function y = getMaxMatrix(x1, x2)
     y = zeros(size(x1));
     
